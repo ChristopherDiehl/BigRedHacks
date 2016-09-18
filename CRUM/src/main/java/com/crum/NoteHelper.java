533c8f2f8f2e2c54a0f7bebbd9c737c428958baa
ChristopherDiehl <csd92@scarletmail.rutgers.edu>
2016-09-17 22:55:46 -0400

    NOtes in db

--- CRUM/src/main/java/com/crum/NoteHelper.java
+++ CRUM/src/main/java/com/crum/NoteHelper.java
@@ -1,15 +1,16 @@
 package com.crum;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class NoteHelper {
 	public static String findGreatestHtmlId(ArrayList<Note> notes)
 	{
-		if (notes == null) 
-			return 0;
+		if (notes == null || notes.size() == 0) 
+			return "0";
 
 		Collections.sort(notes);
-		return notes.get(0);
+		return notes.get(0).getHtmlId();
 
 			
 		
